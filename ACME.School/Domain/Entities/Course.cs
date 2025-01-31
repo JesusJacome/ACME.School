d7e093416e7f385d39b7410861b6119657dd6068
Jesus Jacome <jacomeovallejesus@gmail.com>
2025-01-30 23:05:12 -0500

    Add Student and Course entities

---
+++ ACME.School/Domain/Entities/Course.cs
@@ -0,0 +1,54 @@
+﻿namespace ACME.School.Domain.Entities
+{
+	internal class Course
+    {
+        private const decimal MinFee = 0m;
+
+        public Guid Id { get; } = Guid.NewGuid();
+        public required string Name { get; init; }
+
+        private decimal _registrationFee;
+        public decimal RegistrationFee
+        {
+            get => _registrationFee;
+            private set
+            {
+                if(value < MinFee)
+                {
+                    throw new ArgumentException("Registration Fee cannot be negative", nameof(value));
+				}
+
+                _registrationFee = value;
+            }
+        }
+
+		private DateTime _startDate;
+		private DateTime _endDate;
+
+		public DateTime StartDate => _startDate;
+		public DateTime EndDate => _endDate;
+
+		public Course(string name, decimal registrationFee, DateTime startDate, DateTime endDate)
+        {
+            Name = name;
+            RegistrationFee = registrationFee;
+
+			// Using a method to ensure both dates are set together and validated
+			UpdateCourseDates(startDate, endDate);
+		}
+
+		/// <summary>
+		/// - Ensures `StartDate` and `EndDate` are always valid when updated.
+		/// - Prevents inconsistent state where StartDate and EndDate depend on each other.
+		/// - Instead of setting them individually, both are updated at the same time.
+		/// </summary>
+		public void UpdateCourseDates(DateTime newStartDate, DateTime newEndDate)
+		{
+			if (newEndDate <= newStartDate)
+				throw new ArgumentException("End date must be after the start date.");
+
+			_startDate = newStartDate;
+			_endDate = newEndDate;
+		}
+	}
+}
---
+++ ACME.School/Domain/Entities/Student.cs
@@ -0,0 +1,28 @@
+﻿namespace ACME.School.Domain.Entities
+{
+	internal class Student
+	{
+        private const int MinAge = 18; 
+
+        public Guid Id { get; } = Guid.NewGuid();
+        public required string Name { get; init; }
+        private int _age;
+        public int Age
+        {
+            get => _age;
+            private set
+            {
+                if (value < MinAge)
+                    throw new ArgumentException($"Student must be at least {MinAge} years old", nameof(Age));
+
+				_age = value;
+			}
+        }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+		}
+    }
+}
