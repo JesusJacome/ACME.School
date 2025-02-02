ee44e6845c3fc4fbe949f96e7e1ac2027cc9a231
Jesus Jacome <jacomeovallejesus@gmail.com>
2025-02-01 19:05:44 -0500

    Added simulated Payment gateway

---
+++ ACME.School/Application/Ports/IPaymentGateway.cs
@@ -0,0 +1,13 @@
+﻿using ACME.School.Domain.Entities;
+
+namespace ACME.School.Application.Ports
+{
+	internal interface IPaymentGateway
+	{
+		/// <summary>
+		/// Processes a payment for the given student and course.
+		/// Returns true if the payment is successful.
+		/// </summary>
+		Task<bool> ProcessPaymentAsync(Student student, Course course, decimal amount);
+	}
+}
---
+++ ACME.School/Infrastructure/Adapters/SimulatedPaymentGateway.cs
@@ -0,0 +1,14 @@
+﻿using ACME.School.Application.Ports;
+using ACME.School.Domain.Entities;
+
+namespace ACME.School.Infrastructure.Adapters
+{
+	internal class SimulatedPaymentGateway : IPaymentGateway
+	{
+		public Task<bool> ProcessPaymentAsync(Student student, Course course, decimal amount)
+		{
+			// Simulate a successful payment.
+			return Task.FromResult(true);
+		}
+	}
+}
